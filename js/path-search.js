56f69d6d2972eb6f1a1028e3e0b9bfcff7b625bf
Kammy <liuk@live.unc.edu>
2017-05-28 17:29:41 -0400

    Fix Issue #10 for C term and other path search changes separate selection list and "No Results" text add B term progress count correctly display "no results" for B term hide B term table during C term search

--- js/path-search.js
+++ js/path-search.js
@@ -24,6 +24,7 @@ function triangleSearch(){
 		return;
 	}
 	$(displayText).text("");
+	$("#selections").text("");
 	$("#results").hide();
 	$("#show-subterms").hide();
 	showLoader();
@@ -53,6 +54,7 @@ function triangleSearchOnSuccess(data){
 	showSubresults();
 }	
 	
+var checkedTerms;
 function setFinishSearchHandler(){
 	var selectBar2 = document.getElementById("triType");
 	
@@ -62,31 +64,29 @@ function setFinishSearchHandler(){
 			return;
 		}
 		showLoader()
+		$("#results").hide();
 	
-		var checkedTerms = [];
+		checkedTerms = [];
 		var checkedString = "Your B Terms: ";
-		var term = stack.first;
 		var csvName = "";
-		for(var i=0; i <stack.length;i++){
-			if(term.isSelected){
-				checkedTerms.push(term);
-				checkedString = checkedString + term.name + ", ";
-				csvName = csvName + "_" + term.name;
-			}
-			if(term.right==null){
-				break;
-			}
-			term = term.right;
-		}
+		
+		$("td input:checked").parent().next().each(function(i,el){
+			var term = $(this).text();
+			checkedTerms.push(term);
+			checkedString += term + ", ";
+			csvName += "_" + term;			
+		});
+		
 		checkedString += " Your C Term Type: " + selectBar2.value;
-		$(displayText).text(checkedString);
+		$("#selections").text(checkedString);
 		countER = checkedTerms.length;
-		console.log(countER);
+		//console.log(countER);
 		for(var j=0;j<checkedTerms.length;j++){
 			console.log("Post Request");
 			term = checkedTerms[j];
-			postRequest(term.name,selectBar2.value,newStack,csvName)
+			postRequest(term, selectBar2.value, newStack,csvName)
 		}
+		$(displayText).text("B Terms Done: 0 out of "+checkedTerms.length);
 	};
 }
 	
@@ -96,12 +96,17 @@ function showSubresults(){
 	//makeFilters(stack, "");
 	makeTables(stack,tableLimit,0, SEARCH_TYPE);
 
+	if(stack.length==0){
+		$(displayText).text("No Results");
+		$("#loader").hide();
+		return;
+	}
 	$(loader).hide();
 	$("#results").show();
 	$("#path-subresults").show();
 	$("#downloadform").hide();
 	//$("#filterSection").hide();
-	$(displayText).text("Choose the B Terms you want to search with");
+	$("#selections").text("Choose the B Terms you want to search with");
 
 	newStack = new ThornStack();
 	setFinishSearchHandler();	
@@ -159,52 +164,28 @@ function addTriangleSubTerm(data){
 
 function postRequest(term,type,stack,csvName){
 	var input = document.getElementById("inputbar");
-	var selectBar = document.getElementById("selectBar");
 	var downloadform = document.getElementById("downloadform");
 	
-	var data = "";
-	if(type == "Disease" || type == "Other" || type == "Chemical"){		
-		data = JSON.stringify({		
-			"statements" : [{
-				"statement" : "match (n:Term{name:{name}})-[]-(a)-[]-(m:Term{type:{type}}) return m, a" , "parameters" : {"name": term, "type":type}
-			}]       
-		});
-	}else{
-		data = JSON.stringify({			
-			"statements" : [{
-				"statement" : "match (n:Term{name:{name}})-[]-(a)-[]-(m:Term{stype:{type}}) return m, a" , "parameters" : {"name": term, "type":type}
-			}]       
-		});
-	}
+	var payload = getMentionsByTypePayload(term, type);
 		
-	$.ajax({ //443 works.
-		url: CORS+"http://chemotext.mml.unc.edu:7474/db/data/transaction/commit", //GITHUB PAGES			
-		accepts: "application/json; charset=UTF-8",
-		dataType:"json",
-		contentType:"application/json",		
-		data: data,
-		type:"POST",
-		success:function(data,xhr,status)
-		{
-			console.log("Finished Search");
-			addTermOrSubterm(stack, data);
-			
-			countER--;
-			//$(displayText).text("Percent Done: " + (100-(countER*20))); 
-			//console.log("Count: "+countER);
-			if(countER==0){
-				console.log("FINISHED: "+stack.length)
-				SEARCH_TYPE = "path-final-results";
-				showResult(stack, input.value+"_Path"+csvName, _subterms, SEARCH_TYPE);
-				$("#path-subresults").hide();
-				$("#downloadform").show();
-				$("#show-subterms").hide();
-			}	
-		 },
-		error:function(xhr,err,msg){
-			console.log(err+": "+msg);	
-		}
-	});
+	queryNeo4j(payload, function(data,xhr,status){
+		console.log("Finished Search");
+		addTermOrSubterm(stack, data);
+		
+		countER--;
+		$(displayText).text("B Terms Done: "+(
+			checkedTerms.length-countER)+" out of "+checkedTerms.length); 
+		//console.log("Count: "+countER);
+		if(countER==0){
+			console.log("FINISHED: "+stack.length)
+			SEARCH_TYPE = "path-final-results";
+			$("#results").hide();
+			showResult(stack, input.value+"_Path"+csvName, _subterms, SEARCH_TYPE);
+			$("#path-subresults").hide();
+			$("#downloadform").show();
+			$("#show-subterms").hide();
+		}	
+	 });
 }
 
 function makePathSubresultsTable(stack, index, indexLimit){
@@ -245,10 +226,10 @@ function makePathSubresultsTable(stack, index, indexLimit){
 		node = node.right;
 	}
 	
-	$("td input[type='checkbox']").click(function(){
-		var term = stack.get(this.name);
-		term.isSelected = this.checked;
-	});
+	// $("td input[type='checkbox']").click(function(){
+		// var term = stack.get(this.name);
+		// term.isSelected = this.checked;
+	// });
 }	
 
 
